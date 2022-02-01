75e9f3fb0c266fc8ac6fbd4089a1801859a8715e
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-01 14:52:34 +0000

    Integration Test

--- util.test.js
+++ util.test.js
@@ -1,6 +1,13 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate } = require("./util");
 
+//Unit Test
 test("should return name and age", () => {
+  const text = generateText("Mike", 25);
+  expect(text).toBe("Mike (25 years old)");
+});
+
+//Integration Test
+test("should generate a valid text output", () => {
   const text = generateText("Mike", 25);
   expect(text).toBe("Mike (25 years old)");
 });
